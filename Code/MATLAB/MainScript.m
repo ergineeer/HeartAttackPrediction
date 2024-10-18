2404a5d1f4ed7241db71536028acbd65a5c95f08
Ergin Sezgin <80001281+ergineeer@users.noreply.github.com>
2024-10-18 21:59:29 +0300

    Update MainScript.m

--- Code/MATLAB/MainScript.m
+++ Code/MATLAB/MainScript.m
@@ -17,6 +17,7 @@ size(heartData)
 varTypes = varfun(@class, heartData, 'OutputFormat', 'cell')'
 missingValues = sum(sum(ismissing(heartData)))
 
+
 %% Feature Engineering 
 heartData.CholToAge = heartData.chol./heartData.age;
 gscatter(heartData.age,heartData.CholToAge,heartData.output)
@@ -42,6 +43,7 @@ heartData.STDepressionToMaxRate = heartData.oldpeak./heartData.thalachh;
 gscatter(heartData.age,heartData.STDepressionToMaxRate,heartData.output)
 legend(["Female", "Male"]); grid on; xlabel("Age"); ylabel("Resting Blood Pressure To Number of Major Vessels")
 
+
 %% Datatype Conversion
 heartDataIntact = heartData;
 heartData = movevars(heartData, 'output', 'After', 'STDepressionToMaxRate');
@@ -50,6 +52,7 @@ for i = 1:width(heartData)
         heartData.(i) = double(heartData.(i));
     end
 end
+
 
 %% Correlation Analysis
 heartDataArray = table2array(heartData);
@@ -78,6 +81,7 @@ tickLabels = featImpLabelsbyCorrelation;
 xticklabels(tickLabels);
 grid on
 
+
 %% Feature Analysis by TreeBagger
 heartData_Response = heartData.output;
 heartData_Predictors = removevars(heartData, 'output');
@@ -101,6 +105,7 @@ for i = 1:size(heartData_Predictors,2)
     featImpLabelsbyTreeBagger(i,1) = heartData.Properties.VariableNames{ii}; 
 end
 
+
 %% Feature Analysis by Cross-Tabulation
 pValues = zeros(length(heartData.Properties.VariableNames)-1,1);
 fprintf('\nFeature importance:\n');
@@ -116,3 +121,40 @@ featImpLabelsbypVals = featImpLabelsbypVals(idx,:);
  
 featImp_pValuesOrdered(1) = []; % Remove "Output" Column
 featImpLabelsbypVals(1) = []; 
+
+
+%% Show Prominent Features
+maxLength = max(cellfun(@length, string(heartData.Properties.VariableNames)));
+
+for i=1:15
+    if i==1
+        inputStr = "                                          Important Features\n";
+        inputStr = inputStr + "     Corelation                           Tree Bagger                          Cross-Tabulation\n";
+        inputStr = inputStr + "%.2d - %-*s: %.3f   |   %-*s: %.3f   |   %-*s: %.3f\n";
+    else
+        inputStr = "%.2d - %-*s: %.3f   |   %-*s: %.3f   |   %-*s: %.3f\n";
+    end
+    fprintf(inputStr,i, maxLength, featImpLabelsbyCorrelation(i,1), featImpCorelation(i,1), ...
+        maxLength, featImpLabelsbyTreeBagger(i,1), featImpTreeBagger(i,1), ...
+        maxLength, featImpLabelsbypVals(i,1), featImp_pValuesOrdered(i,1));
+end
+
+trainingFeatures = intersect(featImpLabelsbyTreeBagger, featImpLabelsbyCorrelation)
+trainingFeatures = [trainingFeatures; "output"];
+
+currentColumns = heartDataIntact.Properties.VariableNames;
+columnsToDelete = setdiff(currentColumns, trainingFeatures);
+heartDataIntact(:, columnsToDelete) = [];
+
+
+%% Preparing Train and Test Partitions
+HoldOutRatio = 0.2;
+heartDataPartitions = cvpartition(height(heartDataIntact), "HoldOut", HoldOutRatio)
+
+heartDataTrainingIdx = training(heartDataPartitions);
+heartDataTraining = heartDataIntact(heartDataTrainingIdx,:)
+
+heartDataTestIdx = test(heartDataPartitions);
+heartDataTest = heartDataIntact(heartDataTestIdx,:)
+
+
